4db95d4cce9eff5b2c5717c9ac009ca3ceae5d85
Marotik <kitoram3@gmail.com>
2022-05-09 00:06:35 +0900

    day 2

--- Odoq/settings.py
+++ Odoq/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,9 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
-    'data',
-    'accounts',
+    'main.apps.MainConfig',
+    'data.apps.DataConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    season = models.IntegerField()
+    code = models.CharField(max_length=255)
+    img = models.ImageField()
+    aswr = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.code
