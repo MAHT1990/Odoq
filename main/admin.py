1a3b23a7032110f0648781949d0c157be605ba3d
Marotik <kitoram3@gmail.com>
2022-05-15 01:56:54 +0900

    day two

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display=["code", "season", "img", "aswr"]
+    list_display_links=["code", "season", "img"]
+    
--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    question_models = Question.objects.all().order_by("code")
+    question_first = question_models.first()
+    content = {
+        'code' : question_first.code,
+        'season' : question_first.season,
+        'img' : question_first.img,
+        'aswr' : question_first.aswr
+    }
+    return render(request, 'main/index.html', content)
