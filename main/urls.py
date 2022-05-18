2ee26c1db7d799d6994c4c829e866e5f5b08b69c
Marotik <kitoram3@gmail.com>
2022-05-19 03:00:34 +0900

    day6

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('answer_post/', views.answer_post, name='answer_post')
     ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import *
 
 # Create your views here.
 
-def index(request):
+def current_question():
     question_models = Question.objects.all().order_by("code")
     question_first = question_models.first()
     content = {
@@ -13,4 +14,29 @@ def index(request):
         'img' : question_first.img,
         'aswr' : question_first.aswr
     }
+
+    return content
+
+def index(request):
+    content = current_question()
+
     return render(request, 'main/index.html', content)
+
+def answer_post(request):
+    answer_input = request.POST.get("answer_input")
+    # answer_question_code = request.POST.get("question_code")
+
+    if answer_input==current_question()['aswr']:
+        answer_response = 'Correct'
+
+    else:
+        answer_response = 'Wrong'
+
+    response_data = {
+        'status' : 200,
+        'debugging' : 'Success',
+        'answer_response' : answer_response
+    }
+
+    # return HttpResponseRedirect(reverse('main:index'))
+    return JsonResponse(response_data)
