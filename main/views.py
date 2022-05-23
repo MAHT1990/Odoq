3e3e591171eaf5fcf15d202b94e730ae629dec7d
Marotik <kitoram3@gmail.com>
2022-05-24 01:39:40 +0900

    day 9

--- main/views.py
+++ main/views.py
@@ -8,29 +8,66 @@ from datetime import *
 
 def current_question():
     
-    # Question들의 QuertSet을 불러온다.
+    # Question들의 QuerySet을 불러온다.
     question_queryset = Question.objects.all()
-    rightnow = datetime.now()
+    
+    #현재 시각을 UTC 기준으로 선언한다.
+    #DB에 있는 문제들의 업로드 날짜들은, UTC시각기준이다.
+    rightnow = datetime.now(tz=timezone.utc)
+
+    # 현재 문제와 다음 문제를 확정하기위한 Delta값
+    question_current = None
+    question_next = None
+    question_next_countdown = None
 
     #Debugging
     print("today is", rightnow)
+    print("")
+    # print("today's tzinfo is ", rightnow.tzinfo)
 
     # upload_date와 오늘 현재 시각을 비교하여 data를 확정한다.
     for q in question_queryset:
-        #Debugging
-        print("upload_datetime is", q.upload_datetime)
-        print("type is", type(q.upload_datetime))
-        # print(q.upload_datetime.tzinfo)
-        # question_current = question_queryset.first()
+        
+        #now와의 시간차 계산
+        delta_datetime = rightnow - q.upload_datetime
 
-        if q.upload_datetime.year == rightnow.year and q.upload_datetime.month == rightnow.month and q.upload_datetime.day==rightnow.day:
-            question_current = q
+        #Debugging
+        print("0. question code is ", q)
+        print("1. upload_datetime is", q.upload_datetime)
+        print("2. upload_datetime's tzinfo is", q.upload_datetime.tzinfo)
+        print("3. delta_time is ", delta_datetime)
+        print("")
+
+        #delta_datetime이 0이상이면, 지나간 문제다.
+        #절댓값이 가장 낮은 값이 현재 업로드되어야하는 문제이다.
+
+        if q.upload_datetime >= rightnow:
+            if question_next == None:
+                question_next = q
+                question_next_countdown = delta_datetime.total_seconds()
+
+            else:
+                if q.upload_datetime < question_next.upload_datetime:
+                    question_next = q
+                    question_next_countdown = delta_datetime.total_seconds()
+                else:
+                    None
 
             #Debugging
-            print("today's question is",question_current)
         else:
-            #수정
-            question_current = q
+            if question_current == None:
+                question_current = q
+            else:
+                if q.upload_datetime > question_current.upload_datetime:
+                    question_current = q
+                else:
+                    None
+
+    #Debugging            
+    print("today's question is",question_current)
+    print("next question is ", question_next)
+    print("next question countdowns in seconds is ", -question_next_countdown, "seconds")
+    print(type(question_next_countdown))
 
     content = {
         'code' : question_current.code,
@@ -49,7 +86,7 @@ def index(request):
     content = current_question()
 
     #Debug Log
-    print(content)
+    # print(content)
 
     return render(request, 'main/index.html', content)
 
