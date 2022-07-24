95f28f30460681068397b4af1311ad05df8567e7
Marotik <kitoram3@gmail.com>
2022-07-24 18:59:23 +0900

    login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import (
+    AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm,
+)
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def get_login_form():
+    content = {
+        'login_form' : AuthenticationForm()
+    }
+
+    return content
+
+login = LoginView.as_view(
+        template_name='main/index.html'
+        )
+
+logout = login_required(LogoutView.as_view())
