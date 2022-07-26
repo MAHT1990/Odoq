be6bcd8e54b6b6d232ac2c527415985a69a934a5
Marotik <kitoram3@gmail.com>
2022-07-27 01:47:27 +0900

    LoginView modified

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.forms import (
 
 from .forms import LoginForm, CreationForm
 
-from main.views import get_content
+from main.views import IndexView
 
 User = get_user_model()
 
@@ -24,42 +24,55 @@ User = get_user_model()
 class OdoqLoginView(LoginView):
 
     """ 
+    /accounts/login 으로 get 으로 들어올 때,
+    /accounts/login 으로 POST의 form이 invalid일때,
+
     form_invalid가 main.views의 get_content를 import해서
     main 페이지 load시의 <Dictionary : content>를 받을 수 있도록.
     """ 
+
     def get(self, request, *args, **kwargs):
 
-        main_views = __import__('main.views', fromlist=[''])
-        #DEBUG
+        index = IndexView(get_login_form_bool = False)
+
+        context = self.get_context_data()
+        context.update(index.get_content())
+
+
+        #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG 
+
         debug_title = 'accounts.views.OdoqLoginView.get'
         len_debug_title = len(debug_title)
         print('=' * len_debug_title,'\n',debug_title,'\n','=' * len_debug_title)
 
-        print(super())
-        print(type(self.get_context_data()))
-        print(type(main_views.get_content))
-        print(main_views.get_content())
-        print(self.get_context_data())
-        return self.render_to_response(self.get_context_data())
+        #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG 
+
+
+        return self.render_to_response(context)
 
     def form_invalid(self, form):
 
-        #DEBUG
-        #Debug Log
+        index = IndexView(get_login_form_bool = False)
+
+        context = self.get_context_data()
+        context.update(index.get_content())
+
+
+        #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG
+        
         debug_title = 'accounts.views.OdoqLoginView.form_invalid'
         len_debug_title = len(debug_title)
         print('=' * len_debug_title,'\n',debug_title,'\n','=' * len_debug_title)
-        print(type(self.get_context_data(form=form)))
-        print(self.get_context_data(form=form))
 
-        #TODO 왜 get_content 호출이 불가능한가.
-        #main APP <Class : indexView > 만들어서 다시.
+        #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG #DEBUG
 
-        return self.render_to_response(self.get_context_data(form=form))
+
+        return self.render_to_response(context)
 
 login = OdoqLoginView.as_view(
         template_name='main/index.html'
         )
+
 
 ## LogoutView
 logout = login_required(LogoutView.as_view())
@@ -67,8 +80,9 @@ logout = login_required(LogoutView.as_view())
 
 
 ## CreateView
-signup = CreateView.as_view(
+signin = CreateView.as_view(
     model = User,
     form_class = CreationForm,
+    template_name = 'main/index.html',
     success_url = settings.LOGIN_URL,
 )
