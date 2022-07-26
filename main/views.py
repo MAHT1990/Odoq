566ad2572b0a38f67e753e471f701aa7d9d945b3
Marotik <kitoram3@gmail.com>
2022-07-27 01:48:20 +0900

    create IndexView !!!!

--- main/views.py
+++ main/views.py
@@ -10,204 +10,228 @@ from .models import *
 from .forms import CommentModelForm
 
 from accounts.forms import CreationForm, LoginForm
-######## QUESTION MODEL FILTERING METHODS ########
 
-def get_current_question():
-    #요일 처리
-    seoul_timezone = timezone(timedelta(hours=9))
-    rightnow_kor = datetime.now(tz=seoul_timezone)
-    rightnow_weekday_kor = rightnow_kor.weekday()
+class IndexView:
 
-    #현재 시각을 UTC 기준으로 선언한다. (DB는 UTC 기준)
-    rightnow_utc = datetime.now(tz=timezone.utc)
+    get_current_question_bool = True
+    get_current_notice_bool = True
+    get_login_form_bool = True
+    get_comment_form_bool = True
+
+    ####### CONSTRUCTOR #######
+    def __init__(self, **initkwargs):
+        for key, value in initkwargs.items():
+            setattr(self, key, value)
     
-    # Question들의 QuerySet을 불러온다.
-    question_queryset = Question.objects.all()
+    ######## OnOff MODEL FILTERING METHODS ########
+    def current_OnOff(self):
+        content = {
+            'onoff_season' : None,
+            'season_off_img' : None,
+            'onoff_comment' : None,
+        }
 
-    # 현재 문제와 다음 문제를 확정하기위한 Delta값
-    question_current = None
-    question_next = None
-    question_next_countdown = None
+        #실제로 Query를 날리기 때문에 예외처리를 해준다.
+        try:
+            current_onoff_season = OnOff.objects.all().get(title__icontains="Season")
+            current_onoff_comment = OnOff.objects.all().get(title__icontains="comment")
+        except:
+            return content
 
-    
-    # upload_date와 오늘 현재 시각을 비교하여 data를 확정한다.
-    for q in question_queryset:
+        if current_onoff_season:
+            content['onoff_season'] = current_onoff_season.on_off
+            if current_onoff_season.off_img:
+                content['season_off_img'] = current_onoff_season.off_img
         
-        #now와의 시간차 계산
-        delta_datetime = rightnow_utc - q.upload_datetime
+        if current_onoff_comment:
+            content['onoff_comment'] = current_onoff_comment.on_off
+        
+        return content
 
-        #delta_datetime이 0이상이면, 지나간 문제다.
-        #절댓값이 가장 낮은 값이 현재 업로드되어야하는 문제이다.
+    ######## QUESTION MODEL FILTERING METHODS ########
+    @classmethod
+    def get_current_question(self):
+        #요일 처리
+        seoul_timezone = timezone(timedelta(hours=9))
+        rightnow_kor = datetime.now(tz=seoul_timezone)
+        rightnow_weekday_kor = rightnow_kor.weekday()
 
-        if q.upload_datetime >= rightnow_utc:
-            if question_next == None:
-                question_next = q
-                question_next_countdown = -round(delta_datetime.total_seconds())
+        #현재 시각을 UTC 기준으로 선언한다. (DB는 UTC 기준)
+        rightnow_utc = datetime.now(tz=timezone.utc)
+        
+        # Question들의 QuerySet을 불러온다.
+        question_queryset = Question.objects.all()
 
-            else:
-                if q.upload_datetime < question_next.upload_datetime:
+        # 현재 문제와 다음 문제를 확정하기위한 Delta값
+        question_current = None
+        question_next = None
+        question_next_countdown = None
+
+        
+        # upload_date와 오늘 현재 시각을 비교하여 data를 확정한다.
+        for q in question_queryset:
+            
+            #now와의 시간차 계산
+            delta_datetime = rightnow_utc - q.upload_datetime
+
+            #delta_datetime이 0이상이면, 지나간 문제다.
+            #절댓값이 가장 낮은 값이 현재 업로드되어야하는 문제이다.
+
+            if q.upload_datetime >= rightnow_utc:
+                if question_next == None:
                     question_next = q
                     question_next_countdown = -round(delta_datetime.total_seconds())
-                else:
-                    None
 
-        else:
-            if question_current == None:
-                question_current = q
+                else:
+                    if q.upload_datetime < question_next.upload_datetime:
+                        question_next = q
+                        question_next_countdown = -round(delta_datetime.total_seconds())
+                    else:
+                        None
+
             else:
-                if q.upload_datetime > question_current.upload_datetime:
+                if question_current == None:
                     question_current = q
                 else:
-                    None
+                    if q.upload_datetime > question_current.upload_datetime:
+                        question_current = q
+                    else:
+                        None
 
-    #다음문제 없을 때의 처리.
-    if question_next == None:
-        question_next_countdown = 0
+        #다음문제 없을 때의 처리.
+        if question_next == None:
+            question_next_countdown = 0
 
-    #금요일, 토요일, 일요일 일 때의 countdown값을 달리준다.
-    #금요일일 때는 하루 in 초 - 현재 시각 in 초 till 토
-    #토요일일 때는 이틀 in 초 - 현재 시각 in 초 till 월
-    #일요일일 때는 하루 in 초 - 현재 시각 in 초 till 월
+        #금요일, 토요일, 일요일 일 때의 countdown값을 달리준다.
+        #금요일일 때는 하루 in 초 - 현재 시각 in 초 till 토
+        #토요일일 때는 이틀 in 초 - 현재 시각 in 초 till 월
+        #일요일일 때는 하루 in 초 - 현재 시각 in 초 till 월
 
-    if rightnow_weekday_kor == 4 or rightnow_weekday_kor == 5 or rightnow_weekday_kor == 6: #금,토,일요일일 때,
-        t_hour = rightnow_kor.hour
-        t_min = rightnow_kor.minute
-        t_sec = rightnow_kor.second
+        if rightnow_weekday_kor == 4 or rightnow_weekday_kor == 5 or rightnow_weekday_kor == 6: #금,토,일요일일 때,
+            t_hour = rightnow_kor.hour
+            t_min = rightnow_kor.minute
+            t_sec = rightnow_kor.second
 
-        t_day_in_seconds = 86400
-        t_now_in_seconds = 3600 * t_hour + 60 * t_min + t_sec
+            t_day_in_seconds = 86400
+            t_now_in_seconds = 3600 * t_hour + 60 * t_min + t_sec
 
-        if rightnow_weekday_kor == 4: #금요일
-            #24시간을 초로 환산해서 현재시각을 초로 환산한 값을 빼준다.
-            delta_datetime = t_day_in_seconds - t_now_in_seconds 
-    
-        elif rightnow_weekday_kor == 5: #토요일
-            delta_datetime = t_day_in_seconds * 2 - t_now_in_seconds
+            if rightnow_weekday_kor == 4: #금요일
+                #24시간을 초로 환산해서 현재시각을 초로 환산한 값을 빼준다.
+                delta_datetime = t_day_in_seconds - t_now_in_seconds 
         
-        elif rightnow_weekday_kor == 6: #일요일
-            delta_datetime = t_day_in_seconds - t_now_in_seconds 
+            elif rightnow_weekday_kor == 5: #토요일
+                delta_datetime = t_day_in_seconds * 2 - t_now_in_seconds
+            
+            elif rightnow_weekday_kor == 6: #일요일
+                delta_datetime = t_day_in_seconds - t_now_in_seconds 
 
-        question_next_countdown = delta_datetime
+            question_next_countdown = delta_datetime
 
-    
-    #Debugging            
-    # print("today's question is",question_current)
-    # print("next question is ", question_next)
-    # print("next question countdowns in seconds is ", -question_next_countdown, "seconds")
-    # print(type(question_next_countdown))
+        
+        #Debugging            
+        # print("today's question is",question_current)
+        # print("next question is ", question_next)
+        # print("next question countdowns in seconds is ", -question_next_countdown, "seconds")
+        # print(type(question_next_countdown))
 
-    content = {
-        'weekday' : rightnow_weekday_kor,
-        'pk' : question_current.pk,
-        'code' : question_current.code,
-        'season' : question_current.season,
-        'img' : question_current.img,
-        'aswr' : question_current.aswr,
-        'countdown' : question_next_countdown,
-    }
+        content = {
+            'weekday' : rightnow_weekday_kor,
+            'pk' : question_current.pk,
+            'code' : question_current.code,
+            'season' : question_current.season,
+            'img' : question_current.img,
+            'aswr' : question_current.aswr,
+            'countdown' : question_next_countdown,
+        }
 
-    return content
-
-######## NOTICE MODEL FILTERING METHODS ########
-
-def get_current_notice():
-    notice_queryset = Notice.objects.all()
-    current_notice = notice_queryset.first()
-
-    #Debugging
-    # print('notice_queryset is', notice_queryset)
-    # print('Current notice is ', current_notice)
-
-    if current_notice == None:
-        return None
-    else:
-        content = {'notice' : current_notice.img}
         return content
 
-######## OnOff MODEL FILTERING METHODS ########
+    ######## NOTICE MODEL FILTERING METHODS ########
+    def get_current_notice(self):
+        notice_queryset = Notice.objects.all()
+        current_notice = notice_queryset.first()
 
-def current_OnOff():
-    content = {
-        'onoff_season' : None,
-        'season_off_img' : None,
-        'onoff_comment' : None,
-    }
+        #Debugging
+        # print('notice_queryset is', notice_queryset)
+        # print('Current notice is ', current_notice)
 
-    #실제로 Query를 날리기 때문에 예외처리를 해준다.
-    try:
-        current_onoff_season = OnOff.objects.all().get(title__icontains="Season")
-        current_onoff_comment = OnOff.objects.all().get(title__icontains="comment")
-    except:
+        if current_notice == None:
+            return None
+        else:
+            content = {'notice' : current_notice.img}
+            return content
+
+
+    ####### Authentication 관련 #######
+    def get_creation_form(self):
+        content = {
+            'creation_form' : CreationForm()
+        }
+
         return content
 
-    if current_onoff_season:
-        content['onoff_season'] = current_onoff_season.on_off
-        if current_onoff_season.off_img:
-            content['season_off_img'] = current_onoff_season.off_img
-    
-    if current_onoff_comment:
-        content['onoff_comment'] = current_onoff_comment.on_off
-    
-    return content
+    def get_login_form(self):
+        content = {
+            'login_form' : LoginForm()
+        }
 
-####### Authentication 관련 #######
-def get_creation_form():
-    content = {
-        'creation_form' : CreationForm()
-    }
+        return content
 
-    return content
+    ####### Comment 관련 #######
+    def get_comment_form(self):
+        form = CommentModelForm()
 
-def get_login_form():
-    content = {
-        'login_form' : LoginForm()
-    }
+        content = {
+            'comment_form' : form
+        }
+        return content
 
-    return content
+    ###### content : Response에 넣어줄 <Dictionary : content> ######
+    def get_content(self):
+        
+        #1. get_current_question()으로 <Dict : get_current_question()>을 받는다.
+        #2. current_OnOff()으로 <Dict : current_OnOff()>을 추가한다.
+        #3. current_notice()으로 <Dict : current_notice()>을 추가한다.
+        
+        content = dict()
+        content.update(self.current_OnOff())
+        
 
-def get_comment_form():
-    form = CommentModelForm()
+        if self.get_current_question_bool:
+            content.update(self.get_current_question())
+        
+        if self.get_current_notice() != None:
+            content.update(self.get_current_notice())
 
-    content = {
-        'comment_form' : form
-    }
-    return content
+        if self. get_login_form_bool:
+            content.update(self.get_login_form())
 
+        if self.get_comment_form_bool:
+            content.update(self.get_comment_form())
+        
+        #Debug Log
+        debug_title = 'get_content'
+        len_debug_title = len(debug_title)
+        print('=' * len_debug_title,'\n',debug_title,'\n','=' * len_debug_title)
+        print("content is ",content)
+        for key, value in content.items():
+            print(key, '----', value ,'\n')
 
-######## 요청 응답처리 get_content #######
-def get_content():
-    
-    #1. get_current_question()으로 <Dict : get_current_question()>을 받는다.
-    #2. current_OnOff()으로 <Dict : current_OnOff()>을 추가한다.
-    #3. current_notice()으로 <Dict : current_notice()>을 추가한다.
+        return content
 
-    content = get_current_question()
-    content.update(current_OnOff())
-    # content.update(get_comment_form())
-    # content.update(get_login_form())
+    ###### as_view 정의 ######
+    @classmethod
+    def as_view(cls, **initkwargs):
 
-    if get_current_notice() != None:
-        content.update(get_current_notice())
-    
-    #Debug Log
-    debug_title = 'main.views.get_content'
-    len_debug_title = len(debug_title)
-    print('=' * len_debug_title,'\n',debug_title,'\n','=' * len_debug_title)
-    print("content is ",content)
-    for key, value in content.items():
-        print(key, '----', value ,'\n')
+        def view(request, *args, **kwargs):
+            self = cls(**initkwargs)
+            return render(request, 'main/index.html', self.get_content())
 
-    return content
+        return view
 
 ######## 요청 응답처리 VIEWS ########
-@ensure_csrf_cookie
-def index(request):
-    content = get_content()
-    return render(request, 'main/index.html', content)
-    # return render(request, 'main/timer_practice.html')
 
-
-#######
+index = ensure_csrf_cookie(IndexView.as_view())
 
 
 #### 답안 입력 POST 처리 VIEW ####
@@ -216,7 +240,7 @@ def answer_post(request):
     # answer_question_code = request.POST.get("question_code")
 
     if request.method =='POST':
-        if answer_input==get_current_question()['aswr']:
+        if answer_input== IndexView.get_current_question()['aswr']:
             answer_response = 'Correct'
 
         else:
