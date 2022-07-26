0ef8b1ec77ff4e321044f858f8cbba914147266f
Marotik <kitoram3@gmail.com>
2022-07-27 01:47:11 +0900

    signin 관련 url 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
+
+from django.views.generic.base import RedirectView
+
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='main:index',)),
     path('login/',views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('create/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
     ]
