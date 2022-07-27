e7ababa606de53c7cf95785370b368f2ec8d90ae
Marotik <kitoram3@gmail.com>
2022-07-28 02:24:14 +0900

    회원가입을 위해서는 profile도 만들어야 nickname 지정이 가능하다

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(null=False, max_length=20)
