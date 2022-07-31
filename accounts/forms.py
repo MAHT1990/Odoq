208520ed418a52cb77986a9a3f11e318f5c520b2
Marotik <kitoram3@gmail.com>
2022-08-01 03:44:59 +0900

    forms modified

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UsernameField
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-
 from django.utils.translation import gettext, gettext_lazy as _
 
 User = get_user_model()
@@ -16,7 +15,17 @@ class LoginForm(AuthenticationForm):
         'inactive': _("This account is inactive."),
     }
 
-class CreationForm(UserCreationForm):
+class OdoqCreationForm(UserCreationForm):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._meta.model.USERNAME_FIELD in self.fields:
+            self.fields[self._meta.model.USERNAME_FIELD].label = "ID"
+            self.fields[self._meta.model.USERNAME_FIELD].error_messages = {
+                'unique': _("같은 아이디가 이미 존재합니다."),
+            }
+            self.fields[self._meta.model.USERNAME_FIELD].help_text = "문자 / 숫자 / @ / . / + / - / _ 만 사용가능합니다."
+    
     error_messages = {
         'password_mismatch': _('두 비밀번호가 일치하지않습니다.'),
     }
@@ -31,12 +40,6 @@ class CreationForm(UserCreationForm):
         label=_("비밀번호 확인"),
         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
         strip=False,
-        help_text=_("Enter the same password as before, for verification."),
     )
-
-    class Meta:
-        model = User
-        fields = ("username",)
-        field_classes = {'username': UsernameField}
 
     nickname = forms.CharField(label='닉네임', min_length=2, max_length=10)
