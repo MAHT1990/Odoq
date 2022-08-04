23910afe13cac53aa9b5e8f827e3309249156088
Marotik <kitoram3@gmail.com>
2022-08-05 02:27:33 +0900

    대댓글 기능 관련 url mapping

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/',views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('signin/', views.signin, name='signin'),
+    path('profile/', views.userprofile, name='userprofile'),
     ]
