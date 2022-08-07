9983443fb781dbfc9fda3b269f0f5f14d45338e8
Marotik <kitoram3@gmail.com>
2022-08-07 18:07:00 +0900

    UserProfile에 좋아요한 comment 및 cocomment 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-# Create your models here.
-
 from django.contrib.auth import get_user_model
+
+from main.models import Comment, Cocomment
 
 User = get_user_model()
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(null=False, max_length=20)
+
+    like_comments = models.ManyToManyField(Comment, blank=True, related_name='like_users', default=None)
+    like_cocomments = models.ManyToManyField(Cocomment, blank=True, related_name='like_users', default=None)
