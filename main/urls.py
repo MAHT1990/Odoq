a12950fd09d78e3e988b681a3d151153cdc1f683
Marotik <kitoram3@gmail.com>
2022-08-07 22:19:41 +0900

    댓글, 대댓글 관련 url mapping

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('sms_delete/', views.sms_delete, name='sms_delete'),
     path('comment/new/', views.comment_new, name = 'comment_new'),
     path('comment/delete/', views.comment_delete, name = 'comment_delete'),
+    path('comment/like/<int:comment_id>/<int:zero_xor_one>/', views.like, name = 'comment_like'),
     path('cocomment/new/', views.cocomment_new, name = 'cocomment_new'),
     path('cocomment/delete/', views.cocomment_delete, name = 'cocomment_delete'),
+    path('cocomment/like/<int:cocomment_id>/<int:zero_xor_one>/', views.like, name = 'cocomment_like'),
     ]
