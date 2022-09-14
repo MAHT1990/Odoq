50854bdaf80f446e74561654a56f1300b5b20978
Marotik <kitoram3@gmail.com>
2022-09-14 20:44:57 +0900

    baswords filtering added

--- main/forms.py
+++ main/forms.py
@@ -13,7 +13,8 @@ class CommentModelForm(forms.ModelForm):
         bad_words = [
             '씨발', '좆', '씹', '자지', '보지', '개새',
             '씨부랄', '부랄', '씨팔', '니미', '니애미',
-            '느금', '니애비', 'ㅈ같',
+            '느금', '니애비', 'ㅈ같', '샹년', '시발', '시바', '씨바',
+            '씨8', '시8', '십8',
             ]
         for w in bad_words:
             if w in content:
