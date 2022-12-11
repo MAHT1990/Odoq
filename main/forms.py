1164972ee474421c79f0d03041dfd65ac01c8d50
Marotik <kitoram3@gmail.com>
2022-12-11 22:52:14 +0900

    form 간단하게 수정

--- main/forms.py
+++ main/forms.py
@@ -22,22 +22,6 @@ class CommentModelForm(forms.ModelForm):
         return content
 
 
-class CocommentModelForm(forms.ModelForm):
-    class Meta:
-        model = Cocomment
-        fields = ['content']
-        widgets = {
-            'content' : forms.Textarea(attrs={'maxlength' : 500,})
-        }
-    def clean_content(self):
-        content=self.cleaned_data.get('content','')
-        bad_words = [
-            '씨발', '좆', '씹', '자지', '보지', '개새',
-            '씨부랄', '부랄', '씨팔', '니미', '니애미',
-            '느금', '니애비', 'ㅈ같',
-            ]
-        for w in bad_words:
-            if w in content:
-                content = content.replace(w, '*'*len(w))
-        return content
+class CocommentModelForm(CommentModelForm):
+    pass
 
